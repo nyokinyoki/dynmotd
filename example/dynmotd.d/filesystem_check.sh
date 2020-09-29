be7fd82eb62ba44418250808c2663a4bbe239796
Istok Lenarčič <partyzan@gmail.com>
2020-09-29 14:38:08 +0200

    Minor filesystem_check fix
    Grepping mount points was done in a way, that could have more than one match.

--- example/dynmotd.d/filesystem_check.sh
+++ example/dynmotd.d/filesystem_check.sh
@@ -10,6 +10,6 @@ echo "===== Mounts =============================================================
 df | grep ^/dev | tr -s " " | cut -d " " -f 6 | while read -r MOUNT
 do
         dots "$MOUNT"
-        MOUNT_VAL=$(df -Ph | grep "$MOUNT$" | awk '{print $4, "-",$5}' | tr -d '\n')
+        MOUNT_VAL=$(df -Ph | grep " $MOUNT$" | awk '{print $4, "-",$5}' | tr -d '\n')
         echo -e " $COLOR_COLUMN- $MOUNT$RESET_COLORS$DOTSTR: $COLOR_VALUE ${MOUNT_VAL} (avail-use%) $RESET_COLORS"
 done
